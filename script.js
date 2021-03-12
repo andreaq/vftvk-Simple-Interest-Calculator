a545eccc4a7610a8f97093aa53d8eda0c7eec47c
Andrea Quartarone <q.andrea@hotmail.it>
2021-03-12 09:58:56 +0100

    Update script.js

--- script.js
+++ script.js
@@ -4,10 +4,6 @@ function showMessage() {
 
 function compute() {
 	var principal = document.getElementById("principal").value;
-	if(parseInt(principal) < 1) {
-		alert("Enter a positive number");
-		document.getElementById("principal").focus();
-		return;
 	}
 
 
